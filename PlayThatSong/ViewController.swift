70cde3973d7ea5036b67137273efb03805a46468
Ken Wilcox <wilcoxk@gmail.com>
2015-03-29 21:56:25 -0600

    Simple iOS interface

--- PlayThatSong/ViewController.swift
+++ PlayThatSong/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var playButton: UIButton!
+  @IBOutlet weak var currentSongLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func playButtonPressed(sender: UIButton) {
+  }
 
-
+  @IBAction func playPreviousButtonPressed(sender: UIButton) {
+  }
+  
+  @IBAction func playNextButtonPressed(sender: UIButton) {
+  }
 }
 
